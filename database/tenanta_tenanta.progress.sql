6e946ee6b29a2601351a1be09f756fdb6747c9e3
zyarr.innovation <zyarr.innovation@gmail.com>
2025-02-16 23:03:38 +0530

    Database

---
+++ database/tenanta_tenanta.progress.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tenanta
+-- ------------------------------------------------------
+-- Server version	9.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tenanta.progress`
+--
+
+DROP TABLE IF EXISTS `tenanta.progress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tenanta.progress` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Quiz` int NOT NULL,
+  `FillBlanks` int NOT NULL,
+  `TrueFalse` int NOT NULL,
+  `school` int NOT NULL,
+  `standard` int NOT NULL,
+  `student` int NOT NULL,
+  `subject` int NOT NULL,
+  `lesson` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `school` (`school`),
+  KEY `standard` (`standard`),
+  KEY `student` (`student`),
+  KEY `subject` (`subject`),
+  KEY `lesson` (`lesson`),
+  CONSTRAINT `tenanta.progress_ibfk_1` FOREIGN KEY (`school`) REFERENCES `tenanta.school` (`Id`),
+  CONSTRAINT `tenanta.progress_ibfk_2` FOREIGN KEY (`standard`) REFERENCES `tenanta.standard` (`Id`),
+  CONSTRAINT `tenanta.progress_ibfk_3` FOREIGN KEY (`student`) REFERENCES `tenanta.student` (`Id`),
+  CONSTRAINT `tenanta.progress_ibfk_4` FOREIGN KEY (`subject`) REFERENCES `tenanta.subject` (`Id`),
+  CONSTRAINT `tenanta.progress_ibfk_5` FOREIGN KEY (`lesson`) REFERENCES `tenanta.lesson` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tenanta.progress`
+--
+
+LOCK TABLES `tenanta.progress` WRITE;
+/*!40000 ALTER TABLE `tenanta.progress` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tenanta.progress` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-16 23:02:57
---
+++ database/tenanta_tenanta.subject.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tenanta
+-- ------------------------------------------------------
+-- Server version	9.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tenanta.subject`
+--
+
+DROP TABLE IF EXISTS `tenanta.subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tenanta.subject` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `standard` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `standard` (`standard`),
+  CONSTRAINT `tenanta.subject_ibfk_1` FOREIGN KEY (`standard`) REFERENCES `tenanta.standard` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tenanta.subject`
+--
+
+LOCK TABLES `tenanta.subject` WRITE;
+/*!40000 ALTER TABLE `tenanta.subject` DISABLE KEYS */;
+INSERT INTO `tenanta.subject` VALUES (1,'English',1),(2,'Hindi',1),(3,'Marathi',1),(4,'English',2),(5,'Hindi',2),(6,'Marathi',2),(7,'English',3),(8,'Hindi',3),(9,'Marathi',3);
+/*!40000 ALTER TABLE `tenanta.subject` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-16 23:02:53
