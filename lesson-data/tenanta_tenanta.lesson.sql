35a17750d09c65e83d164b7296cca8b77fc2ebb0
zyarr.innovation <zyarr.innovation@gmail.com>
2025-03-14 15:13:18 +0530

    Lesson Data.

---
+++ lesson-data/tenanta_tenanta.lesson.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tenanta
+-- ------------------------------------------------------
+-- Server version	9.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tenanta.lesson`
+--
+
+DROP TABLE IF EXISTS `tenanta.lesson`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tenanta.lesson` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `subject` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `subject` (`subject`),
+  CONSTRAINT `tenanta.lesson_ibfk_1` FOREIGN KEY (`subject`) REFERENCES `tenanta.subject` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tenanta.lesson`
+--
+
+LOCK TABLES `tenanta.lesson` WRITE;
+/*!40000 ALTER TABLE `tenanta.lesson` DISABLE KEYS */;
+INSERT INTO `tenanta.lesson` VALUES (4,'1.Environment',12),(5,'2.Energy Resources',12),(6,'3.Ecosystem and Biodiversity',12),(9,'1.Overview of Operating Systems',13),(10,'2.Services and Components of Operating Systems',13),(11,'3.Process Management',13),(12,'1.Introduction to management concepts and managerial skills',14),(13,'2.Planning and organizing at supervisory level',14),(14,'3.Directing and Controlling at Supervisory level',14),(15,'1.Introduction and Syntax of Python Program',15),(16,'2.Python Operator and Control Flow Statement',15),(17,'3.DataStructures in Python',15);
+/*!40000 ALTER TABLE `tenanta.lesson` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-14 14:47:40
